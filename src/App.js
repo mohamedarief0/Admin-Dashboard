61adffda666190e3a1d7eb8cf1ef2afbe9db1f4a
Arief <mrarief0110@gmail.com>
2024-07-09 11:56:37 +0530

    Route problem fixing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./SideBar/Dashboard";
-import PageNotFound from "./PageNotFound/PageNotFount";
 import LoginForm from "./LoginFormCloudGarage/LoginForm";
-import MailValidation from "./Fogot password/Mailvalidation/MailValidation";
-import InputOtp from "./Fogot password/InputOtp/InputOtp";
-import ResetPassword from "./Fogot password/Reset/Reset";
+import RegistrationForm from "./RegistrationFromCloudGarage/RegistrationFrom";
+
+import Dashboard from "./SideBar/Dashboard";
+import MailValidation from "./FogotPassword/Mailvalidation/MailValidation";
+import InputOtp from "./FogotPassword/InputOtp/InputOtp";
+import ResetPassword from "./FogotPassword/Reset/Reset";
+
+import PageNotFound from "./PageNotFound/PageNotFount";
+
 import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <Routes>
       {/* Routes for login and password reset */}
       <Route path="/" element={<LoginForm />} />
+      <Route path="/registration" element={<RegistrationForm />} />
       <Route path="/mailvalidation" element={<MailValidation />} />
       <Route path="/otp" element={<InputOtp />} />
       <Route path="/resetPassword" element={<ResetPassword />} />
