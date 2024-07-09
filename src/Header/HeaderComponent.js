5b8982d50b4fd03f06eae8f9b024630b657d66ac
Arief <mrarief0110@gmail.com>
2024-07-09 11:48:17 +0530

    Style error fix

--- src/Header/HeaderComponent.js
+++ src/Header/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LogoImg from "../Asset/CG Logo.PNG";
 import "./HeaderComponent.css";
 import { BellOutlined } from "@ant-design/icons";
@@ -6,10 +6,16 @@ import { Link } from "react-router-dom";
 import UserProfile from "../Asset/User-profile.jpg";
 import { Badge } from "antd";
 
-function HeaderComponent({ userData }) {
-  // Sample count, later replace the count
-  const notificationCount = 3;
+function HeaderComponent({ userData, notificationCount, setNotificationCount }) {
+  // // Sample count, later replace the count
+  // const notificationCount = 3;
+ // State to track if notification count should be displayed
+ const [showNotification, setShowNotification] = useState(false);
 
+ // Effect to toggle showNotification based on notification count
+ useEffect(() => {
+   setShowNotification(notificationCount > 0);
+ }, [notificationCount])
   return (
     <div className="logo-container">
       <div className="logo-container-main">
@@ -19,24 +25,22 @@ function HeaderComponent({ userData }) {
           <li>
             <Link to="/dashboard/paymenttracking">
               <Badge
-                count={notificationCount}
-                style={{ backgroundColor: "dodgerblue" }}
+                  count={showNotification ? notificationCount : 0}
+                style={{ backgroundColor: "#02b9f5" }}
               >
                 <BellOutlined style={{ fontSize: "24px" }} />
               </Badge>
             </Link>
           </li>
           <li>
-            <div className="d-flex justify-content-between whole-profile-circle">
-              <div className="circle-profile">
-                <img
-                  src={userData?.profilePhoto || UserProfile }
-                  className="user-profile"
-                  alt="profile-avator"
-                />
-              </div>
-              <div className="admin-name ms-2 justify-content-between">
-                <h6 className="">{userData?.name || "User name"}</h6>
+            <div className="d-flex justify-content-between align-items-center">
+              <img
+                src={userData?.profilePhoto || UserProfile}
+                className="user-profile"
+                alt="profile-avator"
+              />
+              <div className="ms-3 justify-content-between align-items-center profile-details">
+                <h6 className="mb-2">{userData?.name || "User name"}</h6>
                 <p className="person-role">{userData?.role || "Role"}</p>
               </div>
             </div>
